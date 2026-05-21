bf01521cc6214ff2211061b9a9d32c6f7bc851e1
strong25825 <strong25825@github.com>
2026-05-21 23:00:38 +0800

    fix: set correct Supabase URL in client config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // GANTI URL DI BAWAH INI DENGAN PROJECT URL SUPABASE ANDA
-const supabaseUrl = 'https://XXXXXXXXXXXXXXXX.supabase.co'
+const supabaseUrl = 'https://qrxjpbvvqsbtgbferkua.supabase.co';
 const supabaseKey = 'sb_publishable_LV3DB5RFYOEWFMVOmFh1vA_J3JBPwne'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
